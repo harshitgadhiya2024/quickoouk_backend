7fa070d3fe6907433f40d1e6b60a91d30f63beb5
harshitgadhiya2024 <155363296+harshitgadhiya2024@users.noreply.github.com>
2025-06-19 19:03:45 +0530

    changing mail service

---
+++ operations/cache.py
@@ -0,0 +1,22 @@
+import json
+
+import redis
+
+redis_client = redis.Redis(
+    host="167.71.142.207", port=6379, ssl=True, db=0
+)
+
+def get(key):
+    value = redis_client.get(key)
+    if value:
+        return json.loads(value)
+    else:
+        return None
+
+def set_user_cache(key, value):
+    value = json.dumps(value)
+    redis_client.set(key, value)
+
+
+def delete(key):
+    redis_client.delete(key)
