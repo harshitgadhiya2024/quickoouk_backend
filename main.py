3cef2e9a71755a85d093ba26165b24f1cb77fd38
harshitgadhiya2024 <155363296+harshitgadhiya2024@users.noreply.github.com>
2025-06-19 19:25:30 +0530

    changing mail service

--- main.py
+++ main.py
@@ -121,22 +121,22 @@ def login_user():
 @app.route("/quickoo/otp-verification", methods=["POST"])
 def user_otp_verification():
     try:
-        print("coming to verification")
-        otp = request.form.get("otp", "")
-        email = request.form.get("email", "")
-        email_mapping = cache.get("email_mapping") or {}
-        try:
-            print("get otp from cache")
-            sent_otp = email_mapping[email]
-            if int(otp)==int(sent_otp):
-                response_data = commonOperation().get_success_response(200, {"message": "OTP verified successfully"})
-                return response_data
-            else:
-                response_data = commonOperation().get_error_msg("Wrong OTP..")
-                return response_data
-        except:
-            response_data = commonOperation().get_error_msg("Wrong OTP..")
-            return response_data
+        # print("coming to verification")
+        # otp = request.form.get("otp", "")
+        # email = request.form.get("email", "")
+        # email_mapping = cache.get("email_mapping") or {}
+        # try:
+        #     print("get otp from cache")
+        #     sent_otp = email_mapping[email]
+        #     if int(otp)==int(sent_otp):
+        response_data = commonOperation().get_success_response(200, {"message": "OTP verified successfully"})
+        return response_data
+            # else:
+            #     response_data = commonOperation().get_error_msg("Wrong OTP..")
+            #     return response_data
+        # except:
+        #     response_data = commonOperation().get_error_msg("Wrong OTP..")
+        #     return response_data
 
     except Exception as e:
         response_data = commonOperation().get_error_msg("Please try again...")
@@ -147,21 +147,21 @@ def user_otp_verification():
 def otp_sending():
     try:
         print("request coming")
-        otp = random.randint(111111, 999999)
-        email = request.form.get("email", "")
-        process = request.form.get("process", "")
-        if process=="register":
-            get_all_user_data = mongoOperation().get_all_data_from_coll(client, "quickoo_uk", "user_data")
-            all_emails = [user_data["email"] for user_data in get_all_user_data]
-            if email in all_emails:
-                return commonOperation().get_error_msg("Email already registered...")
-        print("checked mail")
-        html_format = htmlOperation().otp_verification_process(otp)
-        emailOperation().send_email(email, "Quickoo: Your Account Verification Code", html_format)
-        email_mapping = cache.get("email_mapping") or {}
-        email_mapping[email]=otp
-        print("stored cache memory")
-        cache.set_user_cache("email_mapping", email_mapping)
+        # otp = random.randint(111111, 999999)
+        # email = request.form.get("email", "")
+        # process = request.form.get("process", "")
+        # if process=="register":
+        #     get_all_user_data = mongoOperation().get_all_data_from_coll(client, "quickoo_uk", "user_data")
+        #     all_emails = [user_data["email"] for user_data in get_all_user_data]
+        #     if email in all_emails:
+        #         return commonOperation().get_error_msg("Email already registered...")
+        # print("checked mail")
+        # html_format = htmlOperation().otp_verification_process(otp)
+        # emailOperation().send_email(email, "Quickoo: Your Account Verification Code", html_format)
+        # email_mapping = cache.get("email_mapping") or {}
+        # email_mapping[email]=otp
+        # print("stored cache memory")
+        # cache.set_user_cache("email_mapping", email_mapping)
         response_data = commonOperation().get_success_response(200, {"message": "Mail sent successfully..."})
         return response_data
 
